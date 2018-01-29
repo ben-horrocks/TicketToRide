09d496e45a12590986d8c04cce89b355d0f25cc3
Ben Horrocks <benjamin.horrocks@byu.edu>
2018-01-29 10:47:40 -0700

    Initial Commit and repo setup

---
+++ app/src/main/java/cs340/tickettoride/LoginActivity.java
@@ -0,0 +1,15 @@
+package cs340.tickettoride;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+  }
+}
