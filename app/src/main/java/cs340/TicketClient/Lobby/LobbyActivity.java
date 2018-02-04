759060a8c5f25f4c1b918553e42cff06e3b90a53
Vibroblade06 <rybro06@byu.edu>
2018-02-04 16:44:34 -0700

    Added methods and documentation to the LobbyPresenter. And began to use those methods to implement updating the GUI's game list

--- app/src/main/java/cs340/TicketClient/Lobby/LobbyActivity.java
+++ app/src/main/java/cs340/TicketClient/Lobby/LobbyActivity.java
@@ -3,6 +3,8 @@ package cs340.TicketClient.Lobby;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,7 +30,7 @@ public class LobbyActivity extends AppCompatActivity {
     private ListView list;
     private GameListAdapter adapter;
 
-    private LobbyPresenter presenter = new LobbyPresenter();
+    private LobbyPresenter presenter = new LobbyPresenter(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,15 +50,24 @@ public class LobbyActivity extends AppCompatActivity {
                 //TODO attempt to join the game that was clicked
             }
         });
+
+        //updates the list of shown games when the filter box loses focus
+        filter.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                List<Game> filteredList = presenter.getFilteredGames(filter.getText().toString());
+                updateGameList(filteredList);
+            }
+        });
     }
 
-    /**
-     * Updates the list of available games in the lobby
-     * @param games
-     */
     public void updateGameList(List<Game> games){
         adapter.openGames = games;
         adapter.notifyDataSetChanged();
+    }
+
+    public String getFilter(){
+        return this.filter.getText().toString();
     }
 
     /**
