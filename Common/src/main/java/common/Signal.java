33a7f6f64aa7b94e0b15c119da2cd97cdbfb42d3
Carter Sloan <cartersloan29@gmail.com>
2018-02-05 16:59:08 -0700

    added signal to Common

--- Common/src/main/java/common/Signal.java
+++ Common/src/main/java/common/Signal.java
@@ -5,4 +5,25 @@ package common;
  */
 
 public class Signal {
+
+    enum SignalType{
+        ERROR, OK, ANNOUNCEMENT
+    }
+
+    private SignalType type;
+    private Object obj;
+
+    Signal(SignalType type, Object obj)
+    {
+        this.type = type;
+        this.obj = obj;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public SignalType getType() {
+        return type;
+    }
 }
