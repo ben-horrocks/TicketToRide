ed0d332d4427a6b08b2e82a4fa7fc9171d7bd152
Carter Sloan <cartersloan29@gmail.com>
2018-02-05 16:57:07 -0700

    Made Common module

--- app/src/main/java/cs340/TicketClient/common/DataModels/GameNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package cs340.TicketClient.common.DataModels;
-
-/**
- * Created by Ben_D on 2/4/2018.
- */
-
-public class GameNotFoundException extends Exception
-{}
--- app/src/main/java/cs340/TicketClient/common/IClient.java
+++
@@ -1,8 +0,0 @@
-package cs340.TicketClient.common;
-
-/**
- * Created by Ben_D on 1/29/2018.
- */
-
-public interface IClient
-{}
