c27ecfc8ab151f99ab3ce49a1fc1eeade35b1516
Ben Horrocks <benjamin.horrocks@byu.edu>
2018-02-09 10:51:13 -0700

    Fixed merge conflict error

--- Common/src/main/java/common/IServer.java
+++ Common/src/main/java/common/IServer.java
@@ -6,13 +6,7 @@ public interface IServer
 {
 	public Signal login(String username, String password);
 	public Signal register(String username, String password, String displayName);
-<<<<<<< HEAD
 	public Signal addGame(String gameName, Player player);
 	public Signal joinGame(Player player , GameID id);
 	public Signal startGame(GameID id);
-=======
-	public Signal startGame(GameID game);
-	public Signal addGame(Game newgame);
-	public Signal JoinGame(GameID id);
->>>>>>> 00fc5948b4c48d0d5c8abc0ec400cadcdc2965d9
 }
