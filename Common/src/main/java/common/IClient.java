1713ba96135d9e29722ae6dc3b5edc0d83525ec9
Carter Sloan <cartersloan29@gmail.com>
2018-02-12 10:39:09 -0700

    documentation for IClient

--- Common/src/main/java/common/IClient.java
+++ Common/src/main/java/common/IClient.java
@@ -10,6 +10,16 @@ import common.DataModels.Signal;
 
 public interface IClient {
 
+    /**
+     * A function call for all clients to update their game list with the provided game list
+     * @param gameList list of games to be pushed to all clients waiting in the lobby
+     */
     void updateGameList(List<GameInfo> gameList);
+
+    /**
+     * A function call for only the clients that have joined the particular game of the provided
+     * game id
+     * @param id a particular game id is provided to identify which game has been started
+     */
     void startGame(GameID id);
 }
