f6eb0596277d3bd61238acc062d78915d0cb625f
jordanHenstrom <jhenstrom@gmail.com>
2018-03-07 10:58:42 -0700

    PacketChanges

--- Common/src/main/java/common/DataModels/StartGamePacket.java
+++ Common/src/main/java/common/DataModels/StartGamePacket.java
@@ -8,25 +8,18 @@ import common.DataModels.GameData.ClientGameData;
 public class StartGamePacket implements Serializable{
 
     private List<Object> initialDestinationCards;
-<<<<<<< HEAD
-=======
     private List<Object> initialTrainCards;
->>>>>>> 1df9bd21a15e5843ad32c9ebc4de5594298f0bb7
     private ClientGameData clientGameData;
 
     public StartGamePacket(List<Object> initialDestinationCards, List<Object> initialTrainCards, ClientGameData clientGameData)
     {
         this.initialDestinationCards = initialDestinationCards;
-<<<<<<< HEAD
-=======
         this.initialTrainCards = initialTrainCards;
->>>>>>> 1df9bd21a15e5843ad32c9ebc4de5594298f0bb7
         this.clientGameData = clientGameData;
     }
 
     public List<Object> getInitialDestinationCards() { return initialDestinationCards; }
 
-<<<<<<< HEAD
     public ClientGameData getClientGameData() {
         return clientGameData;
     }
@@ -35,9 +28,5 @@ public class StartGamePacket implements Serializable{
     {
         return clientGameData.getPlayer().getUser().getUsername();
     }
-=======
     public List<Object> getInitialTrainCards() { return initialTrainCards; }
-
-    public Object getClientGameData() { return clientGameData; }
->>>>>>> 1df9bd21a15e5843ad32c9ebc4de5594298f0bb7
 }
