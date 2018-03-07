87784621682ebe6b2ee4ab2dae6738923c5229a9
Carter Sloan <cartersloan29@gmail.com>
2018-03-07 09:26:43 -0700

    initial commit for chat and history

--- app/src/main/java/cs340/TicketClient/Game/GameModel.java
+++ app/src/main/java/cs340/TicketClient/Game/GameModel.java
@@ -2,9 +2,11 @@ package cs340.TicketClient.Game;
 
 import java.util.List;
 
+import common.DataModels.ChatItem;
 import common.DataModels.DestinationCard;
 import common.DataModels.EdgeGraph;
 import common.DataModels.GameData.Opponent;
+import common.DataModels.HistoryItem;
 import common.DataModels.TrainCard;
 
 public class GameModel
@@ -13,7 +15,9 @@ public class GameModel
 	EdgeGraph gameBoard;
 	List<TrainCard> faceUp;
 	// History history;
+	List<HistoryItem> playHistory;
 	// Chat chat;
+	List<ChatItem> chatMessages;
 
 	boolean isValidDesinationCard(DestinationCard destinationCard)
 	{
@@ -25,5 +29,21 @@ public class GameModel
 	{
 		// TODO: implement canDrawCard
 		return false;
+	}
+
+	public List<HistoryItem> getPlayHistory() {
+		return playHistory;
+	}
+
+	public void setPlayHistory(List<HistoryItem> playHistory) {
+		this.playHistory = playHistory;
+	}
+
+	public List<ChatItem> getChatMessages() {
+		return chatMessages;
+	}
+
+	public void setChatMessages(List<ChatItem> chatMessages) {
+		this.chatMessages = chatMessages;
 	}
 }
