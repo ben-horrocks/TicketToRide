6be082db4202b8d5c0dfa43d4012523f77a36588
Carter Sloan <cartersloan29@gmail.com>
2018-03-07 20:13:16 -0700

    adjusted title of menu fragment holder in game xml

--- app/src/main/java/cs340/TicketClient/GameMenu/ChatPresenter.java
+++ app/src/main/java/cs340/TicketClient/GameMenu/ChatPresenter.java
@@ -2,6 +2,8 @@ package cs340.TicketClient.GameMenu;
 
 import common.DataModels.ChatItem;
 import common.DataModels.GameData.Player;
+import common.DataModels.GameID;
+import cs340.TicketClient.Communicator.ServerProxy;
 import cs340.TicketClient.Game.GameModel;
 
 /**
@@ -20,8 +22,9 @@ public class ChatPresenter {
 
     public void sendChatMessage(String message) {
         Player player = model.getPlayer();
-        ChatItem chat = new ChatItem(player, message);
-        //TODO: send chat message over socket connection
+        GameID id = model.getGameID();
+        ChatItem chatItem = new ChatItem(player, message);
+        ServerProxy.getInstance().send(id, chatItem);
     }
 
     public void updateChatList() {
