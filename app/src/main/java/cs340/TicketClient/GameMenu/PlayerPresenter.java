c08a72ccc590f48dda4c4f9eba8d2af277ce60d2
Kavika <kavikafaleumu@cox.net>
2018-03-09 01:43:58 -0700

    Made a TurnQueue class with restrictive access.

--- app/src/main/java/cs340/TicketClient/GameMenu/PlayerPresenter.java
+++ app/src/main/java/cs340/TicketClient/GameMenu/PlayerPresenter.java
@@ -4,6 +4,8 @@ package cs340.TicketClient.GameMenu;
 import java.util.Queue;
 
 import common.DataModels.GameData.Player;
+import common.DataModels.GameData.TurnQueue;
+import common.DataModels.Username;
 import cs340.TicketClient.Game.GameModel;
 
 public class PlayerPresenter
@@ -64,14 +66,15 @@ public class PlayerPresenter
 
 	public String getTurnQueue()
 	{
-		Queue<Player> queue = model.getGameData().getTurnQueue();
+		TurnQueue queue = model.getGameData().getTurnQueue();
+		Username[] usernames = queue.toArray();
 		StringBuilder sb = new StringBuilder();
 		int position = 1;
-		for (Player p : queue)
+		for (Username username : usernames)
 		{
 			sb.append(String.valueOf(position));
 			sb.append(": ");
-			sb.append(p.getUser().getStringUserName());
+			sb.append(username.toString());
 			sb.append("\n");
 			position++;
 		}
