98a8c0afdb5d9bda419fa83ae70a92f28e53ea0d
jordanHenstrom <jhenstrom@gmail.com>
2018-04-08 20:22:20 -0600

    ReusmeREquest

---
+++ Common/src/main/java/common/request/ResumeRequest.java
@@ -0,0 +1,19 @@
+package common.request;
+
+import common.game_data.GameID;
+import common.player_info.User;
+
+/**
+ * Created by jhens on 4/8/2018.
+ */
+
+public class ResumeRequest {
+    User user;
+    GameID id;
+
+    public ResumeRequest(User user, GameID id)
+    {
+        this.user = user;
+        this.id = id;
+    }
+}
