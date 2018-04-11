c607abd364b1aa01a88fc24b0d2c9127e7e3d34d
Kavika <kavikafaleumu@cox.net>
2018-04-11 10:45:43 -0600

    filling out SQLUserDAO test

--- Server/src/test/java/daos/SQLUserDAOTest.java
+++ Server/src/test/java/daos/SQLUserDAOTest.java
@@ -5,7 +5,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import common.player_info.Password;
+import common.player_info.User;
+import common.player_info.Username;
 import communicators.ConnectionSetup;
 
 import static org.junit.Assert.*;
@@ -16,11 +22,13 @@ import static org.junit.Assert.*;
 public class SQLUserDAOTest
 {
 	private Connection connection;
+	private SQLUserDAO dao;
 
 	@Before
 	public void setUp() throws Exception
 	{
 		this.connection = ConnectionSetup.setup();
+		this.dao = new SQLUserDAO(connection);
 	}
 
 	@After
@@ -32,16 +40,86 @@ public class SQLUserDAOTest
 	@Test
 	public void createTable()
 	{
+		boolean success = dao.createTable();
+		assert(success);
+		try
+		{
+			DatabaseMetaData meta = connection.getMetaData();
+			ResultSet rs = meta.getTables(null, null,
+					SQLUserDAO.UserEntry.TABLE_NAME, new String[] {"TABLE"});
+			if (rs.next())
+			{
+				System.out.println("	TABLE_NAME: " + rs.getString("TABLE_NAME"));
+			}
+			else
+			{
+				assert(false);
+			}
+		}
+		catch (SQLException e)
+		{
+			// Shouldn't really have errors.
+			System.out.println("SQLException when checking if table exists - " + e);
+			assert(false);
+		}
 	}
 
 	@Test
 	public void deleteTable()
 	{
+		boolean success = dao.deleteTable();
+		assert(success);
+		try
+		{
+			DatabaseMetaData meta = connection.getMetaData();
+			ResultSet rs = meta.getTables(null, null,
+					SQLUserDAO.UserEntry.TABLE_NAME, new String[] {"TABLE"});
+			if (rs.next())
+			{
+				assert(false);
+			}
+			else
+			{
+				System.out.println("No such table... Good!");
+			}
+		}
+		catch (SQLException e)
+		{
+			System.out.println("SQLException when checking if table exists - " + e);
+			assert(false);
+		}
 	}
 
 	@Test
 	public void addNewUser()
 	{
+		String test = "Test";
+		String pw = "Password";
+		String anotherTest = "AnotherTest";
+		String panda = "Pa4nda";
+		Username username = new Username(test);
+		Password password = new Password(pw);
+		User user = new User(username, password);
+		boolean success = dao.addNewUser(user);
+		assert(success);
+
+		username = new Username(anotherTest);
+		password = new Password(panda);
+		user = new User(username, password);
+		success = dao.addNewUser(user);
+		assert(success);
+
+		username = new Username(test);
+		password = new Password(pw);
+		User dbUser = dao.getUser(username);
+		user = new User(username, password);
+		assert(dbUser.equals(user));
+
+		username = new Username(anotherTest);
+		password = new Password(panda);
+		dbUser = dao.getUser(username);
+		user = new User(username, password);
+		assert(dbUser.equals(user));
 	}
 
 	@Test
