87e60609655b640cdfb4bf5bc14235ae97a087ad
Vibroblade06 <rybro06@byu.edu>
2018-04-16 10:29:33 -0600

    initial IDatabasePlugin commit

--- Server/src/main/java/plugin/IDatabasePlugin.java
+++ Server/src/main/java/plugin/IDatabasePlugin.java
@@ -1,4 +1,33 @@
 package plugin;
 
+import java.util.List;
+
+import common.communication.Command;
+import common.game_data.GameID;
+import common.game_data.ServerGameData;
+import common.player_info.User;
+import common.player_info.Username;
+import daos.IUserDAO;
+
 public interface IDatabasePlugin {
+    boolean initializeDatabase(boolean cleanSlate);
+
+    User getUser(Username name);
+    List<User> getAllUsers();
+    boolean addUser(User user);
+    boolean deleteUser(Username name);
+    boolean updateUser(User user);
+
+    ServerGameData getGame(GameID id);
+    List<ServerGameData> getRunningGames();
+    List<ServerGameData> getOpenGames();
+    List<ServerGameData> getAllGames();
+    List<ServerGameData> getRunningGames(Username user);
+    boolean addGame(ServerGameData game);
+    boolean deleteGame(GameID id);
+    boolean updateGame(ServerGameData game);
+
+    List<Command> getCommands(GameID id);
+    boolean addCommand(GameID id);
+    /* No Delete commands because the database should handle that by itself */
 }
